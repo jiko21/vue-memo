175e7347baaecfb0350963fba93fb4e21489824c
daikikojima <daikikojima0531@icloud.com>
2019-07-08 23:52:36 +0900

    A: form component

---
+++ src/components/Form.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="form">
+  </div>
+</template>
+<script>
+</script>
+<style scoped>
+.form {
+  display: flex;
+  height: 50px;
+
+  justify-content: space-around;
+  margin: 0 auto;
+    display: flex;
+
+}
+input {
+  font-size: 2.5em;
+  width: 80%;
+}
+button {
+  background-color: #21b9ff;
+  color: #fff;
+  font-size: 1.2em;
+  font-weight: bold;
+  width: 20%;
+  padding: 0;
+}
+.radius {
+  border-radius: 10px;
+}
+</style>
