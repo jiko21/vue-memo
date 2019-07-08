d2aeacb1af20a1329e22deedd60f9966cb27ab83
daikikojima <daikikojima0531@icloud.com>
2019-07-08 23:46:47 +0900

    A: Some css for App Component

--- src/App.vue
+++ src/App.vue
@@ -21,8 +21,13 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin:  0 auto;
+  max-width: 1000px;
+  text-align: center;
+}
+
+h1 {
+  font-size: 3em;
 }
 </style>
